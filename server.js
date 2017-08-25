03985897012d49aa7975d4d7b491bca61ea1ffa8
dotrong <dohieu@gmail.com>
2017-08-24 20:04:15 -0700

    ff

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+//add for authentication
+var passport   = require('passport');
+var session    = require('express-session');
+//end
+
 var db = require('./models');
 var PORT = 3000;
 var app = express();
@@ -10,17 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Add authentication
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+//end
+
 // Static directory
 app.use(express.static("./public"));
 
 require("./routes/user-api-routes")(app);
 require("./routes/location-api-routes")(app);
+//add for authentication
+var authRoute = require('./app/routes/auth.js')(app);
+//end
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> dotro
 db.sequelize.sync().then(function(){
 
     app.listen(PORT,function(){

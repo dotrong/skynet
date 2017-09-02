1184703c0c03feb7c02321018c838d17dd378d3a
dotrong <dohieu@gmail.com>
2017-09-02 02:07:47 -0700

    remove reset db

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ require('./routes/auth.js')(app,passport);
 require('./config/passport/passport.js')(passport,db.User);
 //end
 
-db.sequelize.sync({force:true}).then(function(){
+db.sequelize.sync().then(function(){
 
     app.listen(PORT,function(){
 

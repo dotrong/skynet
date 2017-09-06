544644b7e1819927f40c8d5c54a7f8379f45c471
cmontoya13 <chris@chrismontoya.com>
2017-09-06 11:56:08 -0700

    testing

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -72,7 +72,7 @@ var reload = function() {
             for (var j=0; j<watches.length; j++) {
                 // var type = watches[j].type.toUpperCase();
                 if (watches[j].Alert) { // some watches don't have 'Alert' which causes an error
-                  var title = watches[j].Alert.title);
+                  var title = watches[j].Alert.title;
                   // var alertStatus = watches[j].Alert;
                   var description = watches[j].Alert.description;
                   var dateTime = watches[j].Alert.dateTime;

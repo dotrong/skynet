b5586818371677744ed66c1900b0153ce608d3b4
cmontoya13 <chris@chrismontoya.com>
2017-09-07 10:59:57 -0700

    testing

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -133,7 +133,7 @@ var reload = function() {
 
         $(".delete").click( function(){
           // console.log("DELETE WORKING", id);
-          // console.log($(this));
+          console.log($(this));
           // console.log($(this)[0].id);
           var btnId = $(this)[0].id;
           var delBtnIndex = btnId.match(/\d/).index;

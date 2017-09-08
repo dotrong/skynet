cfc3fe5abbf2b665b72c010b573b3368bd5b4e42
cmontoya13 <chris@chrismontoya.com>
2017-09-08 12:16:26 -0700

    data output screwed up

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -179,10 +179,21 @@ var reload = function() {
                   $("#watchTitle" + i + " .ui-collapsible-heading-toggle").text(city + ", " + state);
                   $("#watchTitle" + i + " .ui-collapsible-heading-toggle").append('<img src="images/trash-can-icon.png" id="delete' + location[i].id + '" class="delete">');
                   $("#watch" + i + " .ui-collapsible-heading-toggle").css("background-color", severity);
-                  
+
                   $("#watch" + i + " div.ui-collapsible-content").append("<div><br>No Alerts</div>");
                 } 
-            } // end of watches for loop
+              }
+              if (severity == null || severity === "Green") {
+                // Fill in dynamic city, state/country data
+                $("#watch" + i + ".ui-content img").attr('src', picture);           
+                $("#watchTitle" + i + " .ui-collapsible-heading-toggle").text(city + ", " + state);
+                $("#watchTitle" + i + " .ui-collapsible-heading-toggle").append('<img src="images/trash-can-icon.png" id="delete' + location[i].id + '" class="delete">');
+                $("#watch" + i + " .ui-collapsible-heading-toggle").css("background-color", severity);
+                
+                $("#watch" + i + " div.ui-collapsible-content").append("<div><br>No Alerts</div>");
+              }
+
+             // end of watches for loop
 
         } // end of locations for loop
 

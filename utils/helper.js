cea4691419cac8c353ec842d09e1c02389cfa361
cmontoya13 <chris@chrismontoya.com>
2017-09-08 15:36:27 -0700

    earthquake date format

--- utils/helper.js
+++ utils/helper.js
@@ -148,9 +148,47 @@ var getEarthQuakeWatch = function(earthquake) {
           var timeDate = $(element).children("updated").text();
           // Date
           var month = timeDate.substring(5, 7);
+          switch (month) {
+            case "01":
+              month = "Jan";
+              break;
+            case "02":
+              month = "Feb";
+              break;
+            case "03":
+              month = "Mar";
+              break;
+            case "04":
+              month = "Apr";
+              break;
+            case "05":
+              month = "May";
+              break;
+            case "06":
+              month = "Jun";
+              break;
+            case "07":
+              month = "Jul";
+              break;
+            case "08":
+              month = "Aug";
+              break;
+            case "09":
+              month = "Sep";
+              break;
+            case "10":
+              month = "Oct";
+              break;
+            case "11":
+              month = "Nov";
+              break;
+            case "12":
+              month = "Dec";
+              break;
+          }
           var day = timeDate.substring(8, 10);
           var year = timeDate.substring(0, 4);
-          var newDate = (year + "-" + month + "-" + day);
+          var newDate = (month + " " + day + ", " + year);
 
           // Time
           var hours = Number(timeDate.substring(11, 13));

972f5d3ec11897b29aca05afd0c7b4b9588cc447
cmontoya13 <chris@chrismontoya.com>
2017-09-08 13:25:04 -0700

    US data issue

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -95,7 +95,7 @@ var reload = function() {
 
             for (var j=0; j<watches.length; j++) {
 
-                if (watches[j].Alert || watches[j].Alert.title != null || watches[j].Alert != null) { // some watches don't have 'Alert' which causes an error
+                if (watches[j].Alert && watches[j].Alert.title != null || watches[j].Alert != null) { // some watches don't have 'Alert' which causes an error
                     var title = watches[j].Alert.title.toUpperCase();
                     var description = watches[j].Alert.description;
                     var dateTime = watches[j].Alert.dateTime;

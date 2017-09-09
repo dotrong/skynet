b6ef1eb196528ab7f971137bb65c23532e53271e
cmontoya13 <chris@chrismontoya.com>
2017-09-09 00:09:32 -0700

    temp weather for all

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -95,12 +95,12 @@ var reload = function() {
 
             for (var j=0; j<watches.length; j++) {
 
-                // Dynamic temperature and conditions
+                var description = watches[j].Alert.description;
                 $("#watch" + i + ".ui-content").append('<div class="temperature">' + description + '</div>');
 
                 if (watches[j].Alert && watches[j].Alert != null && watches[j].Alert.title != null) { // some watches don't have 'Alert' or 'title' which causes an error
                     var title = watches[j].Alert.title.toUpperCase();
-                    var description = watches[j].Alert.description;
+                    
                     var dateTime = watches[j].Alert.dateTime;
                     var external = watches[j].Alert.external;
                     var severity = watches[j].Alert.severity;
@@ -127,6 +127,8 @@ var reload = function() {
                     // console.log(city,state,title,picture,description,dateTime,external,severity);
 
                     // Fill in dynamic city, state/country data
+                    
+
                     $("#watch" + i + ".ui-content img").attr('src', picture);
                     $("#watchTitle" + i + " .ui-collapsible-heading-toggle").text(city + ", " + state);
                     $("#watchTitle" + i + " .ui-collapsible-heading-toggle").append('<img src="images/trash-can-icon.png" id="delete' + id + '" class="delete">');
